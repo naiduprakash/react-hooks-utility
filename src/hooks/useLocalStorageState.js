4d7f42896f642e899b281d1660bccad961f0ce07
prakash-naidu <prakash@quolam.com>
2021-03-19 21:09:07 +0530

    1.0.3

--- src/hooks/useLocalStorageState.js
+++ src/hooks/useLocalStorageState.js
@@ -11,7 +11,7 @@ export default function useLocalStorageState(key, initialValue) {
     return value;
   });
 
-  useEffect(() => {
+  React.useEffect(() => {
     window.localStorage.setItem(key, state);
   }, [state]);
 
